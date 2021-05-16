74e235eac8d885911f82e62ec135691bca5c4110
Arwed Mett <arwed.mett@evocount.de>
2021-05-16 14:26:33 +0200

    removed copyright from test

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,3 @@
-/*
-Copyright (C) EvoCount GmbH - All Rights Reserved
-Unauthorized copying of this file, via any medium is strictly prohibited
-Proprietary and confidential
-
-Arwed Mett <arwed.mett@evocount.de> 2021
-*/
-
 import Signal from "../src"
 import { assert } from "chai"
 
